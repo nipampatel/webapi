5bee4db7f4f7a09e1c4518a528590d82d5e9cc61
Nipam Patel <nipam2000@gmail.com>
2013-10-22 16:25:26 -0400

    First Commit

---
+++ HelloWorldWebApi/HelloWorldWebApi/Controllers/EmployeesController.cs
@@ -0,0 +1,52 @@
+﻿using HelloWorldWebApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace HelloWorldWebApi.Controllers
+{
+    public class EmployeesController : ApiController
+    {
+        private static IList<Employee> employees = new List<Employee>() 
+        { 
+            new Employee()
+            {
+                Id = 12345, FirstName = "John", LastName = "Human"
+            },
+            new Employee()
+            {
+                Id = 12346, FirstName = "Jane", LastName = "Public"
+            },
+            new Employee()
+            {
+                Id = 12347, FirstName = "Joseph", LastName = "Law"
+            }
+        };
+
+        public IEnumerable<Employee> Get()
+        {
+            return employees;
+        }
+
+        public Employee Get(int id)
+        {
+            return employees.FirstOrDefault(e => e.Id == id);
+        }
+
+        public void Post(Employee employee)
+        {
+            int maxId = employees.Max(e => e.Id);
+            employee.Id = maxId + 1;
+            employees.Add(employee);
+        }
+
+        public void Delete(int id)
+        {
+            Employee employee = Get(id);
+            employees.Remove(employee);
+        }
+    }
+}
